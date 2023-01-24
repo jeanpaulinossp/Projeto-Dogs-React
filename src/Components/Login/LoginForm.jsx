c978dc8c341e0606cdc733a32d6c900cbea65635
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-24 17:13:24 -0300

    Criação dos módulos Button e Input para serem reaproveitados no projeto todo

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Post } from "../../api/config";
+import Button from "../Forms/Button/Button";
+import Input from "../Forms/Input/Input";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
@@ -18,17 +20,10 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/create">Cadastro</Link>
     </section>
