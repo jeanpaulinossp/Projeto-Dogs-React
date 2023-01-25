79a459b16d18e5df6369e9b685723fdf5db08b4e
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-25 09:00:13 -0300

    Tratamento da chamada da API do Token e dos dados do usuário

--- src/api/config.jsx
+++ src/api/config.jsx
@@ -1,10 +1,26 @@
-export const Post = (username, password) => {
-  const config = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+export const API_URL = "https://dogsapi.origamid.dev/json";
+
+export const TOKEN_POST = (body) => {
+  return {
+    url: API_URL + "/jwt-auth/v1/token",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     },
-    body: JSON.stringify({ username, password }),
   };
-  return config;
+};
+
+export const USER_GET = (token) => {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
 };
