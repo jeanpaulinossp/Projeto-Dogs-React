e72d18a1a67033d407234516bfb567233835c8ca
jeanpaulinossp <jeanpaulino.ssp@gmail.com>
2023-01-28 16:46:40 -0300

    Feed Modal

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -12,7 +12,7 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link nk to={`/perfil/${photo.author}`}>
               @{photo.author}
             </Link>
