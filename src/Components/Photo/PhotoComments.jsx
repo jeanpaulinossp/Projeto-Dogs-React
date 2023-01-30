0070256edaa423d82fbbf2aaa4121957a4cd865d
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-30 09:45:57 -0300

    Edição da parte de comentários

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,11 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../UserContext";
 import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentSection = useRef(null);
   const { login } = useContext(UserContext);
 
-  return <div>{login && <PhotoCommentsForm id={props.id} />}</div>;
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
